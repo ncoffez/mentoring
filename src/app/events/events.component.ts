372e66ebc2b3bca069ca4ba288bb19fcf3213e93
Michael Pfeuti <michael.pfeuti@ganymede.ch>
2022-08-27 17:34:24 +0200

    event view update

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import events_mock from 'src/mock/event.json'; 
+
 
 @Component({
 	selector: 'app-events',
@@ -6,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
+	events = Object.values(events_mock.events);
+	//events = null;
 	constructor() {}
+ 
+	ngOnInit(): void {	
+		/*
+		var url = "https://www.eventbriteapi.com/v3/events/?event_ids=308606629737,324854607877,331078503717,261358098107";
 
-	ngOnInit(): void {}
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url);
+
+		xhr.setRequestHeader("Authorization", "Bearer VAPLIPCT5TK5HWK22ABA");
+		let _this = this;
+		xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			console.log(xhr.status);
+			let data = JSON.parse(xhr.responseText);
+			_this.events = data.events;
+			console.log(data.events[0].end);
+			console.log(data.events);
+		}};
+		xhr.send();
+		*/
+	}
 }
